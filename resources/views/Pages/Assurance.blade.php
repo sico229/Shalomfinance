458a4b530a51cdff44d6f5c7c1586b650ff5dd07
Sico <serge.aguidissou@gmail.com>
2023-03-27 00:39:27 +0200

    Assurance 2

--- resources/views/Pages/Assurance.blade.php
+++ resources/views/Pages/Assurance.blade.php
@@ -53,35 +53,139 @@
     </section>
 
     <section id="Section3">
-        <h4>DÉCOUVREZ L'ASSURANCE HABITATION SHALOMFINANCE</h4>
+        <div class="container">
+            <h4>DÉCOUVREZ L'ASSURANCE HABITATION SHALOMFINANCE</h4>
 
-        <div>
             <div>
                 <div>
-                    <i class="bi bi-sliders"></i>
-                    <p>Un contrat qui s'adapte à votre besoin</p>
-                </div>
-                <p>Des garanties essentielles et des options adaptées à votre situation et à vos besoins de protection.
+                    <div>
+                        <i class="bi bi-sliders"></i>
+                        <p>Un contrat qui s'adapte à votre besoin</p>
+                    </div>
+                    <p>Des garanties essentielles et des options adaptées à votre situation et à vos besoins de protection.
 
-                </p>
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <i class="bi bi-ear"></i>
+                        <p>Une assistance dépannage à domicile même sans sinistre</p>
+                    </div>
+                    <p>
+                        Un professionnel intervient 24h/24 et 7j/7 dans votre résidence principale ou secondaire en cas
+                        d'incidents tels que la perte de vos clés, une fuite d'eau ou une panne de votre chaudière. Nous
+                        prenons en charge le déplacement et la 1ère heure de main d'œuvre(2).
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <i class="bi bi-gift-fill"></i>
+                        <p>Votre fidélité récompensée</p>
+                    </div>
+                    <p>
+                        Pas de franchise à régler si votre 1er sinistre a lieu plus de 4 ans après la souscription de votre
+                        contrat(3).
+
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <i class="bi bi-life-preserver"></i>
+                        <p>Une prise en charge financière en cas de coup dur</p>
+                    </div>
+                    <p>
+                        Si l'habitation assurée est votre résidence principale, nous prenons en charge en cas de sinistre
+                        garanti, vos frais de relogement ou le remboursement de vos mensualités de crédit immobilier pendant
+                        24 mois(2).
+
+                    </p>
+                </div>
+
+
             </div>
-            <div>
-                <div>
-                    <i class="bi bi-sliders"></i>
-                    <p>Un contrat qui s'adapte à votre besoin</p>
-                </div>
-                <p>Des garanties essentielles et des options adaptées à votre situation et à vos besoins de protection.
+        </div>
+    </section>
 
-                </p>
-            </div>
-            <div>
-                <div>
-                    <i class="bi bi-sliders"></i>
-                    <p>Un contrat qui s'adapte à votre besoin</p>
-                </div>
-                <p>Des garanties essentielles et des options adaptées à votre situation et à vos besoins de protection.
+    <section id="Section4">
+        <div class="container">
+            <h4><i class="bi bi-megaphone-fill"></i>BON À SAVOIR SI VOUS ROULEZ À VÉLO !</h4>
+            <p>Le contrat Assurance Habitation SHALOMFINANCE (1), vous couvre en responsabilité civile(2) lorsque vous
+                utilisez votre vélo classique ou ou vélo à assistance électrique non immatriculé ne dépassant pas les
+                25km/h. Avec l'option « Vol hors domicile des objets de loisirs » (2), votre vélo est garanti contre le vol
+                jusqu'à 1500€ (4). Pour être protégé en cas d’accident corporel sans tiers responsable,ou pour étendre votre
+                garantie Vol au-delà de 1500€, vous pouvez contacter Cardif IARD gestionnaire de votre contrat Assurance
+                Habitation SHALOMFINANCE via Votre Espace Client Assurance ou par téléphone au n° indiqué dans les documents
+                qui vous ont été remis.
 
-                </p>
+            </p>
+        </div>
+    </section>
+
+    <section id="Section5">
+        <div class="container">
+            <h4>BESOIN D'AIDE SUR L'ASSURANCE HABITATION ?</h4>
+
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <i class="bi bi-chat-left-dots"></i> Comment Souscrire à une assurance habitation SHALOMFINANCE
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>
+                                Pour souscrire à l'Assurance Habitation SHALOMFINANCE, vous devez être client SHALOMFINANCE.
+                                Si
+                                vous souhaitez devenir client SHALOMFINANCE, vous pouvez : Devenir client en ligne. Vous
+                                pourrez ensuite faire votre devis et souscrire directement en ligne en quelques clics.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <i class="bi bi-chat-left-dots"></i>Qui est assuré sur mon contrat d'assurance habitation ?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>
+                                Toutes les personnes qui vivent en permanence à l’adresse assurée* : vous, votre
+                                conjoint(e)/concubin(e)/pacsé(e), vos enfants mineurs. Sont également assurées les personnes
+                                qui vivent occasionnellement avec vous*, comme vos enfants majeurs, âgés de moins de 28 ans
+                                célibataires sans enfant et économiquement à charge. Pour plus d'informations, contactez un
+                                conseiller.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <i class="bi bi-chat-left-dots"></i>Que dois-je faire immédiatement en cas de dégât des eaux ?
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>
+                                Vous devez immédiatement fermer l'arrivée d'eau, couper l'électricité, aérer votre logement,
+                                isoler ou surélever vos meubles ainsi que les appareils électriques et prévenir le voisin,
+                                le syndic ou le gardien si la fuite ne se situe pas chez vous. Pour prévenir et déclarer un
+                                sinistre, rendez-vous ici. Nos conseillers sont aussi disponibles pour répondre à toutes vos
+                                questions.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
