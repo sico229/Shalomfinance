573bf6af7da5cb672bc51dc6a64c64e0ae2b098e
Sico <serge.aguidissou@gmail.com>
2023-03-28 23:40:21 +0200

    Register Page 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use SicoHelpers\Helpers;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function Register(){
+        return Helpers::Viewer('Users.Register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\DevelopperController;
 
